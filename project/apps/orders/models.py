3224abb09bad0634fa0d298d06dcb2ba99e97a6d
demonevilperiodpython <elvidavip954@gmail.com>
2026-01-18 10:36:20 -0500

    web page

--- project/apps/orders/models.py
+++ project/apps/orders/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-
+class caca(models.Model):
+    name = models.CharField(requiered=True ,max_length = 100)
 class model(models.Model):
     name = models.CharField(requiered = False )
-    name = models.DateTimeField(auto_now_Add= True)
+    date = models.DateTimeField(auto_now_Add= True)
+    foreing =models.ForeignKey(
+            settings.AUTH_USER_MODEL,
+            on_delete=models.CASCADE,
+            related_name="sessions"
+    )
